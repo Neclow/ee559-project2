c18d556fcf2e8c9a6caf1000cd9159eab9c466ab
Neclow <neil.scheidwasser-clow@epfl.ch>
2020-05-03 15:29:58 +0200

    Typo fix

--- utils.py
+++ utils.py
@@ -6,12 +6,13 @@ def load_data():
     r2 = 1/(2*math.pi)
     trainX = torch.FloatTensor(1000, 2).uniform_(0, 1)
     testX = torch.FloatTensor(1000, 2).uniform_(0, 1)
-    center = torch.FloatTensor([0.5, 0.5])
-    trainY = (trainX.sub_(center).pow(2).sum(axis=1) < r2).long()
-    testY = (testX.sub_(center).pow(2).sum(axis=1) < r2).long()
+    center = torch.FloatTensor([0.5, 0.5]) # Center of the disc
+    trainY = (trainX.sub(center).pow(2).sum(axis=1) < r2).long()
+    testY = (testX.sub(center).pow(2).sum(axis=1) < r2).long()
     
     mean_ = trainX.mean()
     std_ = trainX.std()
+    
     
     #return trainX, to_one_hot(trainY), testX, to_one_hot(testY)
     return standardize(trainX, mean_, std_), to_one_hot(trainY), standardize(testX, mean_, std_), to_one_hot(testY)
